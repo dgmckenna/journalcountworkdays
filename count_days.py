3395bbf52fd76312054ced1e363cf4e6f2791087
dgmckenna <dgmckenna@gmail.com>
2017-04-03 16:27:03 -0400

    Initial commit with starting very basic program

---
+++ count_days.py
@@ -0,0 +1,24 @@
+import re
+import pyperclip
+
+dateRegex = re.compile(r'\d\d\d\d-\d\d-\d\d')
+clipboard = pyperclip.paste()
+
+days_list = dateRegex.findall(clipboard)
+
+print (days_list)
+print ('Number of days', len(days_list))
+
+single_inst_list = []
+
+for item in days_list:
+    count_item = 0
+    for item2 in days_list:
+        if item == item2:
+            count_item +=1
+
+    if count_item == 1:
+        single_inst_list.append(item)
+
+print (single_inst_list)
+print ('Excluding duplicates', len(single_inst_list))
