cf3999a5c771503ca56f722ee71fe414ada21f72
dgmckenna <dgmckenna@gmail.com>
2017-04-06 17:06:15 -0400

    Added duration column

--- count_days.py
+++ count_days.py
@@ -19,14 +19,20 @@ print ('Number of Journal Entries: ', len(regex_days_list))
 wide_output_list = []
 master_tag_list = []
 
-# For each item where we found the regex of the form {YYYY-MM-DD, tag1, tag2}
-# remove the brackets and convert it to a list.
+# Create a list which represents all the non-tag items we put in the header row
+# this is used so we can track how many columns to the right to go before
+# you start putting the tag columns
+header_non_tags = ['Date', 'Duration'] 
+
+# For each item where we found the regex of the form {YYYY-MM-DD, duration, tag1, tag2}
 for item in regex_days_list:
+    #Remove the brackets
     s_item = item[1:-1]
+    
     item_list_tmp = s_item.split(',')
 
     # Perform cleanup on the tags found to remove any leading or ending spaces
-    for tag in item_list_tmp[1:]:
+    for tag in item_list_tmp[len(header_non_tags):]:
         s_tag = tag.strip()
 
         # Add the tag to the master tag list, this is used to build the
@@ -36,11 +42,9 @@ for item in regex_days_list:
         if s_tag not in master_tag_list:
             master_tag_list.append(s_tag)
 
-# Create a list which represents all the non-tag items we put in the header row
-# this is used so we can track how many columns to the right to go before
-# you start putting the tag columns
-header_non_tags = ['Date']        
+       
 header_row = header_non_tags + master_tag_list
+print (master_tag_list)
 output_string = '\t'.join(header_row)
 wide_output_list.append(header_row)
 
@@ -51,9 +55,10 @@ for item in regex_days_list:
     wide_row = [[]] * len(header_row)
     
     # Perform cleanup on the item list to remove any leading or ending spaces
-    date_col_val = item_list_tmp[0]
-    wide_row[0] = date_col_val
-    for tag in item_list_tmp[1:]:
+    for i in range(len(header_non_tags)):
+        wide_row[i] = item_list_tmp[i]
+    
+    for tag in item_list_tmp[len(header_non_tags):]:
         s_tag = tag.strip()
         wide_row[master_tag_list.index(s_tag) + len(header_non_tags)] = 1
     wide_output_list.append(wide_row)
@@ -63,5 +68,4 @@ for item in regex_days_list:
 # into excel or other spreadsheet software
 pyperclip.copy(output_string)
     
-        
-        
+print (output_string)
